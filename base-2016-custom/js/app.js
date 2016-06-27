7a5790d6e4302573ac013d3d941b936ef36628c2
Vince Verhoeven <Vince Verhoeven>
2016-06-26 17:43:09 -0700

    basis

---
+++ base-2016-custom/js/app.js
@@ -0,0 +1,17 @@
+$("#owl-demo").owlCarousel({
+
+    slideSpeed : 1000,
+    paginationSpeed : 800,
+    singleItem:true,
+    autoPlay: 6000,
+    lazyLoad : true,
+    mouseDrag:	true,
+
+    // "singleItem:true" is a shortcut for:
+    // items : 1,
+    // itemsDesktop : false,
+    // itemsDesktopSmall : false,
+    // itemsTablet: false,
+    // itemsMobile : false
+
+});
